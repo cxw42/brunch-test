1afb4ae6f3db27635b6fb4124559d3b83c60cd0c
Chris White <chrisw@leehayes.com>
2018-09-08 12:08:36 -0400

    Put sample module where npm can find it
    This way the master branch's package.json can refer to the simpletestnm
    module using a GitHub URL.

--- wapp/test.js
+++
@@ -1,6 +0,0 @@
-console.log('Toplevel');
-var cowsay=require('cowsay');
-    // The `cowsay` variable is now visible in the developer console ---
-    // it is at the top level.
-console.log(cowsay.say({text: 'Hello, world from toplevel!'}));
-console.log('/Toplevel');
